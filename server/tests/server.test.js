3326baeca9a8830c351fa6623e7d6e5567f2ae9a
VishalRajole <vishal.rajole@mobikontech.com>
2018-10-04 09:36:03 +0530

    delete todo test suit

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,14 +101,40 @@ describe("GET /todos/:id", () => {
   });
 });
 describe("DELETE /todos/:id", () => {
-  var id = "";
   it("should remove document", done => {
+    var hexId = todos[0]._id.toHexString();
+
     request(app)
-      .delete(`/todos/${todos[0]._id.toHexString()}`)
+      .delete(`/todos/${hexId}}`)
       .expect(200)
       .expect(res => {
-        expect(res.body.todo.text).toBe(todos[0].text);
+        expect(res.body.todo._id).toBe(hexId);
       })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(hexId)
+          .then(todo => {
+            expect(todo).toNotExist();
+            done();
+          })
+          .catch(error => {
+            done(error);
+          });
+      });
+  });
+  it("should return 404 if todo not found", done => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it("should return 404 for non-object ids", done => {
+    request(app)
+      .delete("/todos/abcd")
+      .expect(404)
       .end(done);
   });
 });
